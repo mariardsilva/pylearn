73e022c90da95239339f6cac0999204d21773918
Maria Eduarda Rodrigues da Silva <127434584+mariardsilva@users.noreply.github.com>
2025-12-02 16:08:30 +0000

    primeiro modulo

---
+++ modulo_1/E3.py
@@ -0,0 +1,5 @@
+c = float(input("Informe a temperatura em Celsius:"))
+
+f = (c * 1.8) + 32
+
+print("Temperatura em Fahrenheit:", f)
---
+++ modulo_1/E4.py
@@ -0,0 +1,9 @@
+print("---Calculadora de medias---")
+
+num1 = float(input("Digite a primeira nota:"))
+num2 = float(input("Digite a segunda nota:"))
+num3 = float(input("Digite a terceira nota:"))
+
+m = (num1 + num2 + num3) / 3
+
+print("Média das notas:", m)
